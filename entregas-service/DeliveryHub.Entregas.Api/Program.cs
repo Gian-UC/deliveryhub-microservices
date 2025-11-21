7ffa3c2ab2bd0b70036561540b7f34db9e634ddc
Giancarlo Salomone <g.salomone@live.com>
2025-11-21 10:40:37 -0300

    Initial commit: microservices architecture with .NET 8, Docker and YARP gateway

---
+++ entregas-service/DeliveryHub.Entregas.Api/Program.cs
@@ -0,0 +1,19 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger apenas em desenvolvimento
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+
+app.Run();
